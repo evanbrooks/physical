495121bfd34c35e35092ab9e93e9e8ea060f959c
Evan <brooks.ev@gmail.com>
2013-05-06 11:06:29 -0400

    jquery use original browser event

--- script.js
+++ script.js
@@ -36,9 +36,9 @@
   }
 
   function finger(e) {
-    if (e.changedTouches) {
+    if (e.originalEvent.changedTouches) {
       pmouse = mouse;
-      mouse = { x: e.changedTouches[0].pageX, y: e.changedTouches[0].pageY };
+      mouse = { x: e.originalEvent.changedTouches[0].pageX, y: e.originalEvent.changedTouches[0].pageY };
       $("nav").html(mouse.x + " | " + mouse.y);
     }
   }
